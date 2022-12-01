57376c98a53f988dd8604c72843a80527173b29a
Excelsior <anthony.biel98@gmail.com>
2022-12-01 13:32:30 +0100

    CV page added main image, button to open cv

--- personalwebsite_app/blueprints/cv_page/cv_page.py
+++ personalwebsite_app/blueprints/cv_page/cv_page.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
+from personalwebsite_app.jinja2_env import base_structure_jinja2_env
 
 cv_bp = Blueprint(name='cv_page_bp', 
                     import_name=__name__,
@@ -7,6 +8,8 @@ cv_bp = Blueprint(name='cv_page_bp',
                     template_folder='templates/cv_page',
                     url_prefix="/cv");
 
+
 @cv_bp.route('/')
 def cv():
-    return render_template('cv.html');
+    base_html_loader = base_structure_jinja2_env.get_template("base.html");
+    return render_template('cv.html', base_html_loader=base_html_loader);
